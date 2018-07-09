21cf4655896ed7584ac4b50175a41366e57f07bb
george-qiaozhi <george.qiaozhi@gmail.com>
2018-07-09 11:53:47 -0400

    merge folder

---
+++ draid_perf_resilv/s106/ddBench_s106_raidz1_7_3x_1+1.sh
@@ -0,0 +1,18 @@
+# run: ./ddBench.sh &> output/dir 
+# benchmarking draid on s106 with following parity setup
+# DRAM:	125GB
+# Disk: 4TB
+# parity ratio 1:1 x 3 + 1 spare using raidz1
+
+zpool status ztank
+
+# dd 110GB at a time and record MB/s until full, for 11TB pool, take about 100 times
+
+for i in {1..100}
+do
+dd if=/dev/urandom of=/ztank/`date +%m%d%H%M%S`.dat bs=10M count=11000
+done
+
+echo "******ztank status***** " & date +%m-%d-%H-%M:%S
+zpool status ztank
+df -h

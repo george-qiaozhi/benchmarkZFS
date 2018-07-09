decb12a0af49792730f71ccc8de1e08938af67c0
--global <george.qiaozhi@gmail.com>
2018-07-09 13:09:49 -0600

    update script

--- draid_perf_resilv/s106/fillZpool.sh
+++ draid_perf_resilv/s106/fillZpool.sh
@@ -3,5 +3,8 @@
 # step 1: create large file
 fallocate -l 1000G ~/Documents/1t.dat
 
-# step 2: cp the file to zpool
+# step 2: cp the file to zpool to fill 4 TB
+for i in {1..4}
+do
 time cp ~/Documents/1t.dat /tank/`date +%m%d%H%M%S`.dat
+done
